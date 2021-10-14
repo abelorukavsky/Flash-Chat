e09bd0b50bf4721edb86c61208d71086f9217aaa
Anton Belorukavsky <ekstrak@yandex.ru>
2021-10-14 16:02:16 +0300

    Готовое приложение Flash Chat по курсам Udemy

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Anton Belorukavsky on 30.06.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String
+}
